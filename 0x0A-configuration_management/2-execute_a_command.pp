235080f5c48c1f1d1b5a7c8e44acd285047b8336
mai-cs50 <maimohamed.1992@yahoo.com>
2024-02-23 12:34:02 -0800

    s

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
 # reate a manifest that kills a process named killmenow.
 
-exec { 'pkill':
-  command => 'pkill killmenow',
-  provider => 'shell',
+exec { 'killmenow':
+  command     => 'pkill -f killmenow',
+  path        => '/usr/bin:/bin',
+  refreshonly => 'true',
 }
