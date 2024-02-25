8dc0307c888a18a099e2c0a6bba18f42be4a37c6
mai-cs50 <maimohamed.1992@yahoo.com>
2024-02-25 11:39:36 -0800

    s

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -4,4 +4,9 @@ exec { 'killmenow':
   command     => 'pkill -f killmenow',
   path        => '/usr/bin:/bin',
   onlyif      => 'pgrep -f killmenow',
+  refreshonly => true,
+  subscribe   => File['/tmp/killmenow_signal'],
+}
+file { '/tmp/killmenow_signal':
+  ensure => file,
 }
