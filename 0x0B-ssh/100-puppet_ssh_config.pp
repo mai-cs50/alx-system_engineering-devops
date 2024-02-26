0ea921f3e4c0c76dadd21d45986acea70ae48789
mai-cs50 <maimohamed.1992@yahoo.com>
2024-02-26 14:14:05 -0800

    s

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # practice using Puppet
-file-line { 'replace passwordAuthentication':
+file-line { 'ssh_password_authentication':
   ensure  => 'present',
   path    => '/etc/ssh/sshd_config',
   replace => true,
   line    => 'PasswordAuthentication no',
   match   => 'PasswordAuthentication yes',
 }
-file-line {'add the private key ~/.ssh/school':
+file-line {'ssh_identity_file':
   ensure => 'present',
   path   => '~/.ssh/config',
   line   => 'IdentityFile ~/.ssh/school',
