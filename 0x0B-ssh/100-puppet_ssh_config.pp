b044c723c95658d708d97bb04794ba1c9a9c3e08
mai-cs50 <maimohamed.1992@yahoo.com>
2024-07-25 07:26:49 -0700

    a

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,13 +3,14 @@ file {'/etc/ssh/ssh_config':
 	ensure  => 'present',
 }
 
-file-line { 'Turn off passwd auth':
+file_line {'Turn off passwd auth':
   path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no',
   match   => 'PasswordAuthentication yes',
   replace => 'true',
+}
 
-file-line {'ssh_identity_file':
+file_line {'ssh_identity_file':
   ensure => 'present',
   path   => '~/.ssh/config',
   line   => 'IdentityFile ~/.ssh/school',
