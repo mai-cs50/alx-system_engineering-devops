0df1e5a00c66679367afe2fec4fbf08cb0c7ad9f
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-15 12:45:20 -0700

    s

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
-"""Query Reddit API to determine subreddit sub count
-"""
-
-import requests
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    """Request number of subscribers of subreddit
-    from Reddit API
-    """
-    # set custom user-agent
-    user_agent = '0x16-api_advanced-jmajetich'
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
-    # custom user-agent avoids request limit
-    headers = {'User-Agent': user_agent}
-
-    r = requests.get(url, headers=headers, allow_redirects=False)
-
-    if r.status_code != 200:
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
 
-    try:
-        
-        # load response unit from json and return number of subscribers
-        data = r.json().get('data', {})
-        return data.get('subscribers', 0)
-    except (KeyError, ValueError):
-        return 0
-
+    return sub_info.json().get("data").get("subscribers")
